072a7bc90a1bccd3d8f6ae3f1995b8b6b780774f
A329-web <arjeeta9@gmail.com>
2025-08-16 22:20:33 +0530

    final

--- b.py
+++ b.py
@@ -1,20 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+
 root = Tk()
 root.geometry("200x200")
+
 def msg():
-    messagebox.showwarning("Alert", "VIRUS detected!!!!")
-b1 = Button(root, text="click me", command=msg)
-b1.place(x=40, y=80)
+    messagebox.showwarning("Alert","VIRUS detected!!!!")
+
+b1 = Button(root,text="click me",command=msg)
+b1.place(x=40,y=80)
 
 def msg1():
     messagebox.showerror("Error", "Error detected!!!!")
+
 
 b2 = Button(root, text="click me again", command=msg1)
 b2.place(x=40, y=120)
 
 def msg3():
     messagebox.showinfo("Info", "info detected!!!!")
+
+
 b3 = Button(root, text="click me 3", command=msg3)
 b3.place(x=40, y=160)
 
